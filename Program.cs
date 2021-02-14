7402c3cff48dcdd9e7c951fb46d522ef172aa13e
Milkey Tan <819774989@qq.com>
2021-02-14 18:57:44 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Sharprompt;
+using System.Diagnostics;
+using System.Linq;
+
+namespace mRengar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var processes = Process.GetProcesses().ToList().ToDictionary(i => $"{i.ProcessName}:{i.Id}", i => i);
+                var ps = Prompt.Select("Select process you wish to end", processes.Keys);
+                if (Prompt.Confirm("Are you sure to end " + ps)) processes[ps].Kill();
+            }
+        }
+    }
+}
